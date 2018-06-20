334307ef985b66b104534c147f1322e7360037ba
Jessica Schmitz <schmitz.jessical@gmail.com>
2018-06-19 18:41:46 -0700

    add working youtube clone

--- src/index.js
+++ src/index.js
@@ -1,26 +1,46 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 const API_KEY = 'AIzaSyCiv-DiCxxMFinKYAM2DtcK0F754WHPVv0';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
 
 class App extends Component {
 
 constructor(props) {
   super(props);
 
-  this.state= { videos: [] };
+  this.state= {
+    videos: [],
+    selectedVideo: null
+   };
 
-  YTSearch ({key: API_KEY, term: 'surfboards'}, (videos) => {
-    this.setState({ videos });
+   this.videoSearch('surfboards');
+}
+
+videoSearch(term) {
+  YTSearch ({key: API_KEY, term: term}, (videos) => {
+    this.setState({
+      videos: videos,
+      selectedVideo: videos[0]
+    });
   });
 }
 
 
   render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
     return (
       <div>
-        <SearchBar />
+        <SearchBar  onSearchTermChange={videoSearch}/>
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+          videos={this.state.videos} />
       </div>
     );
   }
